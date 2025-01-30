90d611386659f4bd324938372b4aa8c3101b58d2
T00few2 <christian.kjar@gmail.com>
2025-01-30 18:10:08 +0100

    color provider remove

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,10 +1,6 @@
 "use client"
 
 import { ChakraProvider } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
 
 import { createSystem, defaultConfig } from "@chakra-ui/react"
 
